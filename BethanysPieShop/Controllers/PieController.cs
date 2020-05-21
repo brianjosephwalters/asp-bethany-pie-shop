416926935d2319387772957edda75c76a2d322ca
Brian J. Walters <brian.joseph.walters@gmail.com>
2020-05-20 20:03:27 -0500

    Refactored to use ViewModel.

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -22,8 +23,10 @@ namespace BethanysPieShop.Controllers
 
         public ViewResult List()
         {
-            ViewBag.CurrentCategory = "CheeseCake";
-            return View(_pieRepository.AllPies);
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.AllPies;
+            piesListViewModel.CurrentCategory = "Cheese cakes";
+            return View(piesListViewModel);
         }
     }
 }
